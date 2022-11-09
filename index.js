7217eaf02faf2d73b04c685dc88df2132bfe7ee1
Md. Shamiul Islam Shakil <75733493+mdshakil20@users.noreply.github.com>
2022-11-10 00:19:36 +0600

    read operation for all service done

--- index.js
+++ index.js
@@ -15,39 +15,62 @@ app.use(express.json());
 const uri = "mongodb+srv://cloud-kichen:d5TFAy2jShvMHIzq@cluster0.davow0l.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const userCollection = client.db('nodeMongoDB').collection('services');
-        
+
         // read operation for 3 service
-        app.get('/services_3', async(req, res) =>{
+        app.get('/services_3', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
-            const users = await cursor.limit(3).toArray();
-            res.send(users);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
         })
 
         // read operation for all service
-        app.get('/services', async(req, res) =>{
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
-            const users = await cursor.toArray();
-            res.send(users);
+            const services = await cursor.toArray();
+            res.send(services);
         })
-        
+
+        // read operation for service details
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = userCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.post('/services/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const review = req.body;
+            const result = await userCollection.updateOne({ _id: ObjectId(id) },
+                {
+                    $push: {
+                        reviews: {
+                            $each: [review ]
+                        }
+                    }
+                });
+            res.send(result);
+        })
+
     }
-    finally{
+    finally {
 
     }
 
 }
-run().catch(err=> console.error(err));
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('server running');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listen from port ${port}`);
 })
 
